995ed40887f52bc57edc74b24cb038f808afeca4
Alpuss <pharikaschosen@gmail.com>
2019-12-14 17:10:21 +0100

    działa

--- Section_2/bullcowgame/main.cpp
+++ Section_2/bullcowgame/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+
+#include <string>
+
 using namespace std;
+
+void PrintIntro();
+string GetGuessAndPrintBack();
 
 int main() 
 {
-	cout << "Welcome to Bulls and Cows" << endl;
-
+	PrintIntro();
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	
 return 0;
+}
+void PrintIntro() {
+	//przedstawienie gru
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game\n";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+string GetGuessAndPrintBack() {
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
 }
