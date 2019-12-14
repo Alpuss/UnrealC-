fd96ee86fac32cdc88a1486863ce650ebd19dcd8
Alpuss <pharikaschosen@gmail.com>
2019-12-14 18:32:19 +0100

    nadal działa

--- Section_2/bullcowgame/main.cpp
+++ Section_2/bullcowgame/main.cpp
@@ -5,17 +5,33 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
-
+string GetGuess();
+void PlayGame();
+bool AskToPlayAgain();
 int main() 
 {
 	PrintIntro();
-	GetGuessAndPrintBack();
-	GetGuessAndPrintBack();
-	
+	PlayGame();
+	AskToPlayAgain();
 return 0;
 }
-void PrintIntro() {
+void PlayGame() {	
+	//loop
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << endl;
+	}
+}
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again?";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+void PrintIntro()
+{
 	//przedstawienie gru
 	constexpr int WORLD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game\n";
@@ -24,12 +40,12 @@ void PrintIntro() {
 	cout << endl;
 	return;
 }
-string GetGuessAndPrintBack() {
+string GetGuess() 
+{
 	//get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	//repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
 	return Guess;
 }
